a20d08bf875d1363e832c97388a5ae4e068a2344
Daniel <pepelacon@gmail.com>
2023-12-28 11:33:47 -0500

    update and delete users

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,43 @@
+// import {
+// 	Body,
+// 	Controller,
+// 	HttpCode,
+// 	Post,
+// 	UsePipes,
+// 	ValidationPipe,
+// } from '@nestjs/common'
+// import { AuthService } from './auth.service'
+// import { AuthDto } from './dto/auth.dto'
+// import { RefreshTokenDto } from './dto/refreshToken.dto'
+
+// @Controller('auth')
+// export class AuthController {
+// 	constructor(private readonly AuthService: AuthService) {}
+
+// 	@UsePipes(new ValidationPipe())
+// 	@HttpCode(200)
+// 	@Post('login')
+// 	async login(@Body() dto: AuthDto) {
+// 		return this.AuthService.login(dto)
+// 	}
+
+// 	@UsePipes(new ValidationPipe())
+// 	@HttpCode(200)
+// 	@Post('login/access-token')
+// 	async getNewTokens(@Body() dto: RefreshTokenDto) {
+// 		return this.AuthService.getNewTokens(dto)
+// 	}
+
+// 	@UsePipes(new ValidationPipe())
+// 	@HttpCode(200)
+// 	@Post('register')
+// 	async register(@Body() dto: AuthDto) {
+// 		return this.AuthService.register(dto)
+// 	}
+// }
+
 import {
+	BadRequestException,
 	Body,
 	Controller,
 	HttpCode,
@@ -6,9 +45,9 @@ import {
 	UsePipes,
 	ValidationPipe,
 } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthDto } from './dto/auth.dto'
 import { RefreshTokenDto } from './dto/refreshToken.dto'
+import { AuthDto } from './dto/auth.dto'
+import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
@@ -17,21 +56,26 @@ export class AuthController {
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login')
-	async login(@Body() dto: AuthDto) {
-		return this.AuthService.login(dto)
+	async login(@Body() data: AuthDto) {
+		return this.AuthService.login(data)
 	}
 
 	@UsePipes(new ValidationPipe())
 	@HttpCode(200)
 	@Post('login/access-token')
-	async getNewTokens(@Body() dto: RefreshTokenDto) {
-		return this.AuthService.getNewTokens(dto)
+	async getNewTokens(@Body() data: RefreshTokenDto) {
+		return this.AuthService.getNewTokens(data)
 	}
 
 	@UsePipes(new ValidationPipe())
-	@HttpCode(200)
 	@Post('register')
 	async register(@Body() dto: AuthDto) {
+		const oldUser = await this.AuthService.findByEmail(dto.email)
+		if (oldUser)
+			throw new BadRequestException(
+				'User with this email is already in the system'
+			)
+
 		return this.AuthService.register(dto)
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,106 @@
-import {
-	BadRequestException,
-	Injectable,
-	UnauthorizedException,
-} from '@nestjs/common'
-import { ModelType } from '@typegoose/typegoose/lib/types'
-import { InjectModel } from 'nestjs-typegoose'
-import { UserModel } from 'src/user/user.model'
-import { AuthDto } from './dto/auth.dto'
-import { hash, compare, genSalt } from 'bcryptjs'
+// import {
+// 	BadRequestException,
+// 	Injectable,
+// 	UnauthorizedException,
+// } from '@nestjs/common'
+// import { ModelType } from '@typegoose/typegoose/lib/types'
+// import { InjectModel } from 'nestjs-typegoose'
+// import { UserModel } from 'src/user/user.model'
+// import { AuthDto } from './dto/auth.dto'
+// import { hash, compare, genSalt } from 'bcryptjs'
+// import { JwtService } from '@nestjs/jwt'
+// import { RefreshTokenDto } from './dto/refreshToken.dto'
+
+// @Injectable()
+// export class AuthService {
+// 	constructor(
+// 		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+// 		private readonly jwtService: JwtService
+// 	) {}
+
+// 	async login(dto: AuthDto) {
+// 		const user = await this.validateUser(dto)
+// 		const tokens = await this.issueTokenPair(String(user._id))
+// 		return {
+// 			user: this.returnUserField(user),
+// 			...tokens,
+// 		}
+// 	}
+
+// 	async getNewTokens({ refreshToken }: RefreshTokenDto) {
+// 		if (!refreshToken) throw new UnauthorizedException('Please sign in!')
+
+// 		const result = await this.jwtService.verifyAsync(refreshToken)
+// 		if (!result) throw new UnauthorizedException('Invalid token or expires!')
+
+// 		const user = await this.UserModel.findById(result._id)
+// 		const tokens = await this.issueTokenPair(String(user._id))
+// 		return {
+// 			user: this.returnUserField(user),
+// 			...tokens,
+// 		}
+// 	}
+
+// 	async register(dto: AuthDto) {
+// 		const oldUser = await this.UserModel.findOne({ email: dto.email })
+// 		if (oldUser)
+// 			throw new BadRequestException(
+// 				'User with this email is already in the system'
+// 			)
+
+// 		const salt = await genSalt(10)
+// 		const newUser = new this.UserModel({
+// 			email: dto.email,
+// 			password: await hash(dto.password, salt),
+// 		})
+
+// 		const tokens = await this.issueTokenPair(String(newUser._id))
+// 		return {
+// 			user: this.returnUserField(newUser),
+// 			...tokens,
+// 		}
+// 	}
+
+// 	async validateUser(dto: AuthDto): Promise<UserModel> {
+// 		const user = await this.UserModel.findOne({ email: dto.email })
+// 		if (!user) throw new UnauthorizedException('User not found')
+
+// 		const isValidPassword = await compare(dto.password, user.password)
+// 		if (!isValidPassword) throw new UnauthorizedException('Invalid password')
+
+// 		return user
+// 	}
+
+// 	async issueTokenPair(userId: string) {
+// 		const data = { _id: userId }
+// 		const refreshToken = await this.jwtService.signAsync(data, {
+// 			expiresIn: '15d',
+// 		})
+// 		const accessToken = await this.jwtService.signAsync(data, {
+// 			expiresIn: '1h',
+// 		})
+
+// 		return { refreshToken, accessToken }
+// 	}
+
+// 	returnUserField(user: UserModel) {
+// 		return {
+// 			_id: user._id,
+// 			email: user.email,
+// 			isAdmin: user.isAdmin,
+// 		}
+// 	}
+// }
+
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { genSalt, hash, compare } from 'bcryptjs'
+import { InjectModel } from 'nestjs-typegoose'
 import { RefreshTokenDto } from './dto/refreshToken.dto'
+
+import { AuthDto } from './dto/auth.dto'
+import { UserModel } from '../user/user.model'
 
 @Injectable()
 export class AuthService {
@@ -18,11 +109,29 @@ export class AuthService {
 		private readonly jwtService: JwtService
 	) {}
 
-	async login(dto: AuthDto) {
-		const user = await this.validateUser(dto)
+	async login({ email, password }: AuthDto) {
+		const user = await this.validateUser(email, password)
+
 		const tokens = await this.issueTokenPair(String(user._id))
+
 		return {
-			user: this.returnUserField(user),
+			user: this.returnUserFields(user),
+			...tokens,
+		}
+	}
+
+	async register({ email, password }: AuthDto) {
+		const salt = await genSalt(10)
+		const newUser = new this.UserModel({
+			email,
+			password: await hash(password, salt),
+		})
+		const user = await newUser.save()
+
+		const tokens = await this.issueTokenPair(String(user._id))
+
+		return {
+			user: this.returnUserFields(user),
 			...tokens,
 		}
 	}
@@ -31,41 +140,28 @@ export class AuthService {
 		if (!refreshToken) throw new UnauthorizedException('Please sign in!')
 
 		const result = await this.jwtService.verifyAsync(refreshToken)
-		if (!result) throw new UnauthorizedException('Invalid token or expires!')
+
+		if (!result) throw new UnauthorizedException('Invalid token or expired!')
 
 		const user = await this.UserModel.findById(result._id)
+
 		const tokens = await this.issueTokenPair(String(user._id))
+
 		return {
-			user: this.returnUserField(user),
+			user: this.returnUserFields(user),
 			...tokens,
 		}
 	}
 
-	async register(dto: AuthDto) {
-		const oldUser = await this.UserModel.findOne({ email: dto.email })
-		if (oldUser)
-			throw new BadRequestException(
-				'User with this email is already in the system'
-			)
-
-		const salt = await genSalt(10)
-		const newUser = new this.UserModel({
-			email: dto.email,
-			password: await hash(dto.password, salt),
-		})
-
-		const tokens = await this.issueTokenPair(String(newUser._id))
-		return {
-			user: this.returnUserField(newUser),
-			...tokens,
-		}
+	async findByEmail(email: string) {
+		return this.UserModel.findOne({ email }).exec()
 	}
 
-	async validateUser(dto: AuthDto): Promise<UserModel> {
-		const user = await this.UserModel.findOne({ email: dto.email })
+	async validateUser(email: string, password: string): Promise<UserModel> {
+		const user = await this.findByEmail(email)
 		if (!user) throw new UnauthorizedException('User not found')
 
-		const isValidPassword = await compare(dto.password, user.password)
+		const isValidPassword = await compare(password, user.password)
 		if (!isValidPassword) throw new UnauthorizedException('Invalid password')
 
 		return user
@@ -73,9 +169,11 @@ export class AuthService {
 
 	async issueTokenPair(userId: string) {
 		const data = { _id: userId }
+
 		const refreshToken = await this.jwtService.signAsync(data, {
 			expiresIn: '15d',
 		})
+
 		const accessToken = await this.jwtService.signAsync(data, {
 			expiresIn: '1h',
 		})
@@ -83,7 +181,7 @@ export class AuthService {
 		return { refreshToken, accessToken }
 	}
 
-	returnUserField(user: UserModel) {
+	returnUserFields(user: UserModel) {
 		return {
 			_id: user._id,
 			email: user.email,
