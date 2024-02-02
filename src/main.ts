906efd15ed1b95e70e5e1ed673d7c994aad77361
Daniel <pepelacon@gmail.com>
2024-02-02 10:50:16 -0500

    Adjust heroku-postbuild script for Angular build

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.enableCors({
-		origin: 'https://front-next-fixjw0lm2-pepelacon.vercel.app', // Replace with your Vercel app URL
+		origin: '*', // Replace with your Vercel app URL
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 	})
