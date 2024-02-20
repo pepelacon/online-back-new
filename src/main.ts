b1b16b7543ea756fdb84ba322d6a02eb36d80204
Daniel <pepelacon@gmail.com>
2024-02-20 11:18:16 -0500

    specify host

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
 	app.enableCors({
-		origin: 'https://front-next-fixjw0lm2-pepelacon.vercel.app', // Replace with your Vercel app URL
+		origin: 'https://front-next-set.vercel.app', // Replace with your Vercel app URL
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 	})
